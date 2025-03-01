8665a7b14013f08a8b61d13be7b18032ce821fb4
Fabricyo <fazin_c@hotmail.com>
2025-03-01 22:06:53 +0000

    Primeiras modificações

---
+++ src/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    let menu = document.getElementById("menu-list");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
